a68da100a6f991be71bb0a2a417215477623100d
Mikael Lindström <mikael.lindstrom@lindstromsmanagement.se>
2023-03-10 13:58:04 +0100

    Add start comments and welcome the user to this program/game

--- Program.cs
+++ Program.cs
@@ -2,11 +2,25 @@
 
 namespace ArenaFighter
 {
+    /// <summary>
+    /// The start class for this program/game
+    /// </summary>
     internal class Program
     {
+        // Name of this program/game
+        const string ProgramName = "The Arena Fighter 2000";
+        
+        /// <summary>
+        /// The start point of this program/game
+        /// </summary>
+        /// <param name="args">Anything</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Welcome the user to this program/game
+            Console.WriteLine($"Welcome to {ProgramName}!");
+
+            // Make a pause in this program/game
+            Console.ReadKey();
         }
     }
 }
