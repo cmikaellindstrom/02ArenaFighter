da59f5934ac87126ecb6ce27ba5bcaa588f0f39a
Mikael Lindström <mikael.lindstrom@lindstromsmanagement.se>
2023-03-11 10:51:00 +0100

    Create character for the player and their opponent in the battle and start the battle

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace ArenaFighter
         // Name of this program/game
         const string ProgramName = "The Arena Fighter 2000";
 
+        // A array of names for opponent character
+        static string[] names = new string[6] {"David" , "Valter", "Erik", "Paul", "Gustav", "Karl"};
+
         // Instans that handle creating of randomly values
         internal static Random random = new Random();
         
@@ -24,6 +27,24 @@ namespace ArenaFighter
 
             // Make a pause in this program/game
             Console.ReadKey();
+
+            // Ask the user about a name of their character then create a character
+            Console.Write("Write a name of your character: ");
+            string playerName = Console.ReadLine();
+
+            Character player = new Character(playerName);
+
+            while(player.IsAlived == true && player.IsRetired == false)
+            {
+                // Character a randomly opponent to the player
+                Character opponent = new Character(names[random.Next(1, names.Length)]);
+
+                // Create a new Battle
+                Battle battle = new Battle(player, opponent);
+
+                // Make a pause in this program/game
+                Console.ReadKey();
+            }
         }
     }
 }
